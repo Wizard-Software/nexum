d99de1e7f826849a17166b672ae81a5e531b5760
github-action-benchmark <github@users.noreply.github.com>
2026-02-15 00:52:36 +0000

    add Benchmark (benchmarkdotnet) benchmark result for d7790c8ef1fc3f30e7acba544e49cc660851ddac

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771104685712,
+  "lastUpdate": 1771116756228,
   "repoUrl": "https://github.com/asawicki/nexum",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 133.76622319221497,
             "unit": "ns",
             "range": "± 0.2740335938025817"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "artur.sawicki@gmail.com",
+            "name": "asawicki",
+            "username": "asawicki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7790c8ef1fc3f30e7acba544e49cc660851ddac",
+          "message": "Merge pull request #3 from asawicki/MinimalApis\n\nAdd NuGet package metadata, versioning, and SourceLink support in Dir…",
+          "timestamp": "2026-02-15T01:51:11+01:00",
+          "tree_id": "9736eee49555bddaa10b28b4c8d368f1b2aa40f1",
+          "url": "https://github.com/asawicki/nexum/commit/d7790c8ef1fc3f30e7acba544e49cc660851ddac"
+        },
+        "date": 1771116755841,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Nexum.Benchmarks.NexumRegressionBenchmarks.Nexum_SimpleCommand",
+            "value": 77.99936705827713,
+            "unit": "ns",
+            "range": "± 2.4431979550406604"
+          },
+          {
+            "name": "Nexum.Benchmarks.NexumRegressionBenchmarks.Nexum_3Behaviors",
+            "value": 196.89222939809164,
+            "unit": "ns",
+            "range": "± 1.1423601108130668"
+          },
+          {
+            "name": "Nexum.Benchmarks.NexumRegressionBenchmarks.Nexum_5NotificationHandlers",
+            "value": 166.64211948712668,
+            "unit": "ns",
+            "range": "± 0.3739126286195366"
           }
         ]
       }
